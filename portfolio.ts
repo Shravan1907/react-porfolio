cfed7ee442da7548ec3308ce126c42eab7eb2758
Shravan1907 <skshravan1907@gmail.com>
2024-07-05 22:13:34 +0530

    I updated my resume

--- portfolio.ts
+++ portfolio.ts
@@ -16,7 +16,7 @@ export const greetings: GreetingsType = {
   title: "Hi all, I'm Shravan",
   description:
     "I'm a passionate Full Stack web developer with experience developing Full Stack web applications with Java, MongoDB, React.js, CSS, Nodejs, and mySQl. I am strongly interested in learning new technologies and implementing them in my projects. I'm a self-motivated and hardworking individual who is always ready to learn new things and work in a team.",
-  resumeLink: "https://drive.google.com/file/d/1y5fXvRqqs_n7eEd3z45xsKQox8FBxbV8/view",
+  resumeLink: "https://drive.google.com/file/d/1-kWGMbSCmtZBUYCB22VtBbF27ikmogln/view?usp=drive_link",
 };
 
 export const openSource = {
